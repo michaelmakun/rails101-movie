7cd0ede5096161138e3fa1c66e7764cf2d2ec2c8
makun <michaelmakun@aliyun.com>
2017-03-23 13:27:21 +0800

    implement movie CRUD

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,48 @@
+class MoviesController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @movies = Movie.all.order("created_at DESC")
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path, notice: "电影更新成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, alert: "电影删除成功"
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title,:description)
+  end
+end
